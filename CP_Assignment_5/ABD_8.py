3ab9d7dccf52d9a3330a6b5a64edba55aa37ad65
ABD-01 <63636498+ABD-01@users.noreply.github.com>
2020-06-14 19:28:16 +0000

    brakets

--- CP_Assignment_5/ABD_8.py
+++ CP_Assignment_5/ABD_8.py
@@ -5,7 +5,7 @@ n = int(input("Enter the number : "))
 print(n, end=" ")
 while n != 1:
     if n % 2:
-        n = 3 * n + 1
+        n = (3 * n) + 1
     else:
         n = n // 2
     print(n, end=" ")
